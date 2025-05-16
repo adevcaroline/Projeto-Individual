67b5ffff5c1b84618eb5e5603d8bef46aacac81d
adevcaroline <caroline.soares@sptech.school>
2025-05-16 15:24:11 -0300

    Adicionando Script e API

---
+++ src/database/script-tabelas.sql
@@ -0,0 +1,25 @@
+CREATE DATABASE hp;
+
+USE hp;
+
+CREATE TABLE usuario (
+    idusuario INT PRIMARY KEY,
+    nomeCompleto VARCHAR(45),
+    Email VARCHAR(80),
+    senha VARCHAR(20)
+);
+
+CREATE TABLE Casa (
+    idCasa INT PRIMARY KEY,
+    nomeCasa VARCHAR(45),
+    descricao VARCHAR(45)
+);
+
+CREATE TABLE CasaIdeal (
+    fkusuario INT,
+    fkCasa INT,
+    dtResposta DATE,
+    PRIMARY KEY (fkusuario, fkCasa),
+    FOREIGN KEY (fkusuario) REFERENCES usuario(idusuario),
+    FOREIGN KEY (fkCasa) REFERENCES Casa(idCasa)
+);
